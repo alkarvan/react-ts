d1914dcf843815465593c8c0605c70e7a3fcf4c4
OLEKSANDR KARVAN <al.karvan@gmail.com>
2024-03-13 00:00:19 +0200

    TS4-AsyncThunk

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {Profiler} from 'react';
 import ReactDOM from 'react-dom/client';
+import {RouterProvider} from "react-router-dom";
 
 import './index.css';
-import {App} from './App';
+import {router} from "./router";
+import {Provider} from "react-redux";
+import {store} from "./store";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
+    <Provider store={store}>
+        <RouterProvider router={router}/>
+    </Provider>
 
 );
 
---
+++ src/pages/carsPage.tsx
@@ -0,0 +1,13 @@
+import {CarForm, Cars} from "../components/carsContainer";
+
+const CarsPage = () => {
+    return (
+        <div>
+            <CarForm/>
+            <hr/>
+            <Cars/>
+        </div>
+    );
+};
+
+export {CarsPage};
